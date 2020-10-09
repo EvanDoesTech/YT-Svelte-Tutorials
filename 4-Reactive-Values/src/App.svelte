2b8cbeb1e63bfb36776591efa8af292f0036ad3e
DevAscend <71956581+DevAscend@users.noreply.github.com>
2020-10-09 11:58:26 -0400

    Add files via upload

---
+++ 4-Reactive-Values/src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	let firstName = "";
+	let lastName = "";
+	let src = "favicon.png"
+
+	$: fullName = `${firstName} ${lastName}`
+	$: {
+		console.log(lastName);
+		console.log(firstName);
+	}
+
+</script>
+
+<main>
+	<h1>Hello {fullName}</h1>
+
+	<label for="firstname">FirstName</label>
+	<input type="text" bind:value={firstName}>
+	<label for="firstname">LastName</label>
+	<input type="text" bind:value={lastName}>
+
+	
+	<img {src} alt="Svelte logo">
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	h1 {
+		font-size: 72px;
+		text-align: center;
+	}
+
+	h3 {
+		font-size: 38px;
+	}
+</style>
