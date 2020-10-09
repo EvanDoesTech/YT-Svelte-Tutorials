c98c5441da5562c5f54dcb95499311c9e3dcc9a9
DevAscend <71956581+DevAscend@users.noreply.github.com>
2020-10-09 12:07:58 -0400

    Add files via upload

---
+++ 3-Input-Handling-and-Binding/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	let name = "";
+	let src = "favicon.png";
+	let count = 0;
+
+	const incrementCounter = () => {
+		count += 1;
+	}
+
+	const handleInput = (event) => {
+		name = event.target.value;
+	}
+
+	const resetName = () => {
+		name = "Unknown";
+	}
+
+</script>
+
+<main>
+	<h1>Hello {name}</h1>
+
+	<input type="text" bind:value={name}>
+	<button on:click={resetName}>Set name to Unknown</button>
+
+	<img {src} alt="Svelte logo">
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	h1 {
+		font-size: 72px;
+		text-align: center;
+	}
+
+	h3 {
+		font-size: 38px;
+	}
+</style>
