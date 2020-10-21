aa99f838927fb6788606a12556ae962145ccbe0a
DevAscend <71956581+DevAscend@users.noreply.github.com>
2020-10-21 17:13:49 -0400

    Add files via upload

---
+++ 12-Event-Modifiers/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Button from "./Button.svelte";
+
+	const log = () => {
+		console.log("Element clicked")
+	}
+</script>
+
+<main>
+	<Button on:click={log} />
+	<button on:click|once={log}>App button</button> 
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
